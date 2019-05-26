55ade9d6149abcee574da80adf20f2870c3daab0
andydevs <akanshul97@gmail.com>
2019-05-26 18:30:41 -0400

    Created app

--- register_me_silly/__init__.py
+++ register_me_silly/__init__.py
@@ -6,3 +6,31 @@ Notifies me when classes are available
 Author:  Anshul Kharbanda
 Created: 5 - 26 - 2019
 """
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Declare SQLALchemy
+
+# ClassCheck model
+    # class id (string)
+    # time id (string)
+    # url (string)
+    # available (bool)
+
+# ClassCheck form
+    # class id
+    # time id
+    # url
+
+# ClassCheck list view
+@app.route('/')
+def index():
+    """
+    Class check list view
+    """
+    return render_template('index.html')
+
+# Check classes async task
+    # Notify when done
+# Trigger every 15 minutes
