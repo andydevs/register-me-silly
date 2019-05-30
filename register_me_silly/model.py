849885c3830179dccc0605b3ba3dedc0b77453e5
andydevs <akanshul97@gmail.com>
2019-05-30 15:34:03 -0400

    Actually needed to keep the migrations repo

--- register_me_silly/model.py
+++ register_me_silly/model.py
@@ -17,12 +17,16 @@ class ClassCheck(db.Model):
     """
     Class Check Model
     """
+    __tablename__ = 'class_check'
+
+    # Fields
     id = db.Column(db.Integer(), primary_key=True)
     class_id = db.Column(db.String())
     time_id = db.Column(db.String())
     url = db.Column(db.String())
     available = db.Column(db.Boolean(), default=False)
     last_checked = db.Column(db.Date())
+
 
 class NewClassCheckForm(FlaskForm):
     """
