8fd07d4b9735baa4c3cd57ff8317911585be2755
andydevs <akanshul97@gmail.com>
2019-06-05 11:34:26 -0400

    Added queue-check-class command

--- register_me_silly/cli.py
+++ register_me_silly/cli.py
@@ -6,18 +6,28 @@ Notifies me when classes are available
 Author:  Anshul Kharbanda
 Created: 5 - 26 - 2019
 """
+from click import argument
 from sqlalchemy.orm import load_only
 from . import app
-from .tasks import check_classes, test_tasks
+from .tasks import check_classes, check_class, test_tasks
 from .model import ClassCheck
 
 
 @app.cli.command()
 def queue_check_classes():
     """
-    Check class record with the given id
+    Check all class records
     """
     check_classes.delay()
+
+
+@app.cli.command()
+@argument('id', type=int)
+def queue_check_class(id):
+    """
+    Check class record with the given id
+    """
+    check_class.delay(id)
 
 
 @app.cli.command()
